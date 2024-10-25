6520898e9c852f138d1741b2b9e438262eecbdef
szholbarys <sakenzholbarys@gmail.com>
2024-10-25 23:10:07 +0500

    Initial Commit

---
+++ MyFavCollections/MyFavCollectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavCollectionsApp.swift
+//  MyFavCollections
+//
+//  Created by Zholbarys on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFavCollectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
